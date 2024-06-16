a561685c85ce3d3606c0e54563ab7f9b021d50a1
luizfelipe17 <luizfeliperabelo17@gmail.com>
2024-06-16 02:01:25 -0300

    Delete LisTudo directory

--- LisTudo/src/main/java/interfaces/I_LisTudo.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-import metodos.LoginMetodos;
-
-public interface I_LisTudo {
-
-    public void telaInicial(LoginMetodos loginMetodos);
-    public void opcoes(int opcao, LoginMetodos loginMetodos);
-    
-}
